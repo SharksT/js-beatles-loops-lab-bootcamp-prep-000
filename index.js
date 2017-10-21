21befd7c180c2c729e61de20c71432437245e5b0
Felipe Passos <felipekaires@hotmail.com>
2017-10-21 18:16:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+
+function theBeatlesPlay(musicians1,instruments1) {
+
+var array['']
+  for (var i = 0; i < musicians.length; i++) {
+    array.push( musicians1[i] +"plays"+ $instruments1[i])
+  }
+  return array
+}
